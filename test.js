6743ad3fd0b52972c349c06b4b2b5bb464f1decd
krutha <80782192+krutha@users.noreply.github.com>
2021-06-28 17:31:18 -0400

    Update test.js

--- test.js
+++ test.js
@@ -3,6 +3,6 @@ const getGreeting = require('./project');
 test('can create a basic greeting', () => {
   expect(getGreeting('Sam')).toBe("Hi Sam");
 })
-test('say hello',function(){
-  expect(sayHello('sai')).toBe('Hi sai');
+test('can create a basic greeting',function(){
+  expect(getGreeting('sai')).toBe('Hi sai');
 });
